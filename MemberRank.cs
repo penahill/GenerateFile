aa3c045c786d38f5165c7f1a136a594721f5fafa
penahill <50646068+penahill@users.noreply.github.com>
2019-05-15 19:49:24 -0600

    Create MemberRank.cs

---
+++ MemberRank.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distro
+{
+    public class MemberRank
+    {
+        public int MemberID { get; set; }
+        public DateTime RankDate { get; set; }
+        public int RankID { get; set; }
+    }
+}
