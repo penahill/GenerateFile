28f28f9e98fd362c492c9484a4bce8ec0d69857b
penahill <50646068+penahill@users.noreply.github.com>
2019-05-15 19:48:35 -0600

    Create MemberProfile.cs

---
+++ MemberProfile.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distro
+{
+    public class MemberProfile
+    {
+        public int MemberId { get; set; }
+        public DateTime JoinDate { get; set; }
+        public string MemberType { get; set; }
+        public string CountryCode { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PhoneNumberFlag { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string EducationLevel { get; set; }
+        public string MaritalStatus { get; set; }
+        public string NumberOfResidents { get; set; }
+        public string HouseType { get; set; }
+        public string Occupation { get; set; }
+        public string HouseholdIncomeLevel { get; set; }
+        public string Religion { get; set; }
+        public string PreferredLanguage { get; set; }
+        public string PreferredChannelOfcontact { get; set; }
+        public string MemberStatus { get; set; }
+
+    }
+}
